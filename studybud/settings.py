d5a9d79ceed0037fa07d3c93759efbc61e0a2dee
yawsaii <bernardsaii20@gmail.com>
2022-01-17 15:02:12 +0000

    deploy fix-3

--- studybud/settings.py
+++ studybud/settings.py
@@ -145,7 +145,7 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
